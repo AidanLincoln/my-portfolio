2175582fb9becb077ae3910cc75953f449a9c202
Aidan Lincoln <aidantlincoln@gmail.com>
2023-11-24 23:50:46 -0800

    cleaned up animation timings. added about, projects, and contact component files. added hover effects for socials buttons

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,13 +2,19 @@ import Anime from 'react-anime';
 
 export default function Description() {
     return(
-        <Anime translateY={[20,0]} opacity={[0,1]} easing={"easeInSine"} duration={500} delay={0} >
-            <div className="descriptionContainer">           
+        <div className="descriptionContainer">
+            <Anime translateY={[10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={800} >           
                 <div className="descriptionText1">Hi, my name is</div>
+            </Anime>
+            <Anime translateY={[10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={900} >   
                 <div className="descriptionText2">Aidan Lincoln.</div>
+            </Anime>
+            <Anime translateY={[10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={1000} > 
                 <div className="descriptionText3">I create better digital experiences.</div>
+            </Anime>
+            <Anime translateY={[10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={1100} > 
                 <div className="descriptionText4">I'm a software engineer based in Seattle. blah blah balh blah blah nlah ujhbagjwg agwag gw.</div>    
-            </div>
-        </Anime>
+            </Anime>
+        </div>
 );
 }
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,27 +1,31 @@
 import Anime from 'react-anime';
 
 export default function Socials(){
-    return(
-        <Anime opacity={[0,1]} easing={"easeInSine"} duration={300} delay={800}>
-            <div className='socialsContainer'>
-                <ul className='socialsUL'>
-                    <li className='socialsLI'>    
+    return( 
+        <div className='socialsContainer'>
+            <ul className='socialsUL'>
+                <li className='socialsLI'>  
+                    <Anime translateY={[-10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={1600}>  
                         <a href='https://github.com/AidanLincoln' target="_blank" rel="noopener noreferrer">
                             <img src={require("../assets/github-icon.png")} className="socialIcon" alt="githubIcon"></img>   
                         </a>
-                    </li>  
-                    <li className='socialsLI'>
+                    </Anime>
+                </li>  
+                <li className='socialsLI'>
+                    <Anime translateY={[-10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={1500}>  
                         <a href='https://www.linkedin.com/in/aidan-lincoln/' target="_blank" rel="noopener noreferrer">
                             <img src={require("../assets/linkedin-icon.png")} className="socialIcon" alt="linkedinIcon"></img>      
                         </a>
-                    </li>   
-                    <li className='socialsLI'>
+                    </Anime>
+                </li>   
+                <li className='socialsLI'>
+                    <Anime translateY={[-10,0]} opacity={[0,1]} easing={"easeInSine"} duration={300} delay={1400}>
                         <a href="mailto:aidantlincoln@gmail.com" target="_blank" rel="noopener noreferrer">
                             <img src={require("../assets/email-icon.png")} className="socialIcon" alt="emailIcon"></img>      
                         </a>
-                    </li>   
-                </ul>  
-            </div>       
-        </Anime>
+                    </Anime>
+                </li>   
+            </ul>  
+        </div>       
     )
 }
