7591a82bc10731423c233a7e5e3da85888a322bd
Aidan Lincoln <aidantlincoln@gmail.com>
2023-11-29 16:51:21 -0800

    Started writing the HTML and CSS for the about, projects, and contact components.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,12 @@
 export default function Contact() {
     return (
-        <div>
-            
+        <div className="contactContainer">
+            <div className="contactTitle">
+                Contact
+            </div>
+            <div className="contactText">
+                I'd love to hear from you, let's talk and make something happen!
+            </div>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,10 @@
 export default function Projects() {
     return (
-        <div>
-            
+        <div className="projectsContainer">
+            <div className="projectsTitle">Projects</div>
+            <div className="project"></div>
+            <div className="project"></div>
+            <div className="project"></div>
         </div>
     )
 }
